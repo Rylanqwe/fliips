f5105aa3809c7d5b5e3deef1fbd69bf5b1cd9ee5
Rylanqwe <122843110+Rylanqwe@users.noreply.github.com>
2023-01-20 21:12:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ document.getElementById("my-button").addEventListener("click", function() {
 document.getElementById("my-button").addEventListener("click", function() {
   document.getElementById("my-element").style.backgroundColor = "red";
 });
+def handle_bet(user_bet):
+    outcome = random.randint(0, 1)
+    if user_bet == outcome:
+        return "win"
+    else:
+        return "lose"
+def validate_bet(user_bet, user_balance):
+    if user_bet > user_balance:
+        return "Insufficient balance"
+    elif user_bet <= 0:
+        return "Invalid bet amount"
+    else:
+        return "Valid"
+def store_result(user_address, bet_amount, outcome, payout):
+    new_result = BetResult(user_address=user_address, bet_amount=bet_amount, outcome=outcome, payout=payout)
+    db.session.add(new_result)
+    db.session.commit()
