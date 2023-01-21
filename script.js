8eade1d706bdd2af429e36f70d560109af680ed0
Rylanqwe <122843110+Rylanqwe@users.noreply.github.com>
2023-01-20 20:35:54 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("my-button").addEventListener("click", function() {
+  document.getElementById("my-text").innerHTML = "Button was clicked!";
+});
+document.getElementById("my-button").addEventListener("click", function() {
+  document.getElementById("my-element").style.backgroundColor = "red";
+});
